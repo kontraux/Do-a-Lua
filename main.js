74ec816ce1c71cf759564dde616abc92d80c88a9
Sam <samuelblake@gmail.com>
2022-08-10 11:56:47 -0500

    added themes

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = get_theme()
+var mode = sessionStorage.getItem("theme", mode) || "dark_mode"
+
+function get_theme() {
+    document.body.classList.remove("theme", mode)
+    var mode = sessionStorage.getItem("theme", mode)
+    document.body.classList.add("theme", mode)
+    return mode
+}
+
+theme.onclick = function () {
+    document.body.classList.remove("theme", mode)
+    if (mode === "light_mode") {
+        mode = "dark_mode"
+    } else { mode = "light_mode"}
+    document.body.classList.add("theme", mode)
+    sessionStorage.setItem("theme", mode)
+    return mode
+}
